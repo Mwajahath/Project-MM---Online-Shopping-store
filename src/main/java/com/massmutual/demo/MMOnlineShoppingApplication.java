0a9ca4145157fbeb75b37cd708645e0cba4ff95e
Mwajahath <wajahath845@gmail.com>
2023-01-02 10:16:33 +0530

    code cleaning

--- src/main/java/com/massmutual/demo/MMOnlineShoppingApplication.java
+++ src/main/java/com/massmutual/demo/MMOnlineShoppingApplication.java
@@ -2,19 +2,15 @@ package com.massmutual.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableSwagger2
 public class MMOnlineShoppingApplication {
 
 	public static void main(String[] args) {
 		
-//		SpringApplication.run(MMOnlineShoppingApplication.class, args);
-		ApplicationContext context= SpringApplication.run(MMOnlineShoppingApplication.class, args);
+		SpringApplication.run(MMOnlineShoppingApplication.class, args);
 	
 	}
 
